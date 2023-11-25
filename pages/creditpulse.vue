b01aedb691264734c61e42ab4ea368b6a6f95d14
nicholaslwj000918 <148545570+nicholaslwj000918@users.noreply.github.com>
2023-11-25 10:11:36 +0800

    Add files via upload

---
+++ pages/creditpulse.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+TableComp
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import TableComp from '~/components/TableComp.vue'
+
+export default {
+  name: 'CreditPulse',
+  components: {
+    TableComp
+  },
+  layout: 'dashboardLayout',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters({})
+  },
+  methods: {}
+}
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      // logoPath: require('../assets/logo/FincareLogo2.png')
+    }
+  },
+  created () {
+    setTimeout(() => this.$router.push({ path: '/creditpulse' }), 1000)
+  },
+  methods: {
+
+  }
+}
+</script>
